14cfe888ea36a5dce713ca3a1a4d41c26ac05599
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-09-06 14:05:49 +0900

    fix: gradle permission

---
+++ src/main/generated/com/file/transfer/domain/file/entity/QFileEntity.java
@@ -0,0 +1,59 @@
+package com.file.transfer.domain.file.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QFileEntity is a Querydsl query type for FileEntity
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QFileEntity extends EntityPathBase<FileEntity> {
+
+    private static final long serialVersionUID = -1901169404L;
+
+    public static final QFileEntity fileEntity = new QFileEntity("fileEntity");
+
+    public final com.file.transfer.global.entity.QBaseEntity _super = new com.file.transfer.global.entity.QBaseEntity(this);
+
+    //inherited
+    public final StringPath createdBy = _super.createdBy;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdDate = _super.createdDate;
+
+    public final StringPath fileName = createString("fileName");
+
+    public final StringPath filePath = createString("filePath");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath originalName = createString("originalName");
+
+    public final StringPath password = createString("password");
+
+    //inherited
+    public final StringPath updatedBy = _super.updatedBy;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedDate = _super.updatedDate;
+
+    public QFileEntity(String variable) {
+        super(FileEntity.class, forVariable(variable));
+    }
+
+    public QFileEntity(Path<? extends FileEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QFileEntity(PathMetadata metadata) {
+        super(FileEntity.class, metadata);
+    }
+
+}
+
