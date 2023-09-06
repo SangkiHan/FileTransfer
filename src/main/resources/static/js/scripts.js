6923db63485429006f1b8a3a7e8ab4de3bf4d632
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-09-06 14:42:29 +0900

    fix: progress bar

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -9,7 +9,6 @@ function openModal(fileId) {
 function closeModal() {
     var modal = document.getElementById("passwordModal");
     modal.style.display = "none";
-    location.reload();
 }
 
 function checkPassword() {
@@ -39,4 +38,38 @@ function checkPassword() {
 		    alert(errorData.errorMessage);
         }
     });
+}
+
+function uploadFile() {
+    var fileInput = document.getElementById("file");
+    var progressBar = document.getElementById("progress-bar");
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/upload", true);
+
+    xhr.upload.onprogress = function (event) {
+        if (event.lengthComputable) {
+            var percentComplete = (event.loaded / event.total) * 100;
+            progressBar.style.width = percentComplete + "%";
+        }
+    };
+
+    xhr.onload = function () {
+        // 파일 업로드 완료 후 서버에서의 응답을 처리
+        if (xhr.status === 200) {
+            // 파일 업로드 성공
+            console.log("파일 업로드 성공");
+        } else {
+            // 오류 처리
+            console.error("파일 업로드 실패");
+        }
+    };
+
+    var formData = new FormData();
+    formData.append("file", fileInput.files[0]);
+    // 필요한 경우 다른 폼 데이터를 추가합니다.
+
+    xhr.send(formData);
+
+    return false; // 폼이 일반적으로 제출되는 것을 방지합니다.
 }
