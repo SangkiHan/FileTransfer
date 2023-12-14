cd063b97d7238dd6286dc5611d34037913ba3540
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-12-14 16:49:17 +0900

    fix: context-path 수정

--- src/main/java/com/file/transfer/FileTransferApplication.java
+++ src/main/java/com/file/transfer/FileTransferApplication.java
@@ -2,6 +2,8 @@ package com.file.transfer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -24,5 +26,10 @@ public class FileTransferApplication {
 	@Bean
 	JPAQueryFactory jpaQueryFactory(EntityManager em) {
 		return new JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager);
+	}
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>
+	webServerFactoryCustomizer() {
+		return factory -> factory.setContextPath("/filetransfer");
 	}
 }
